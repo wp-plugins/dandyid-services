6ff9e39c237eff4848cdb5afb14b999cb64b4b4b
nsimon <nsimon@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-01-05 03:31:51 +0000

    Converted SimpleXLMElement() calls to xml_parse() calls for PHP4 compatibility.
    git-svn-id: https://plugins.svn.wordpress.org/dandyid-services/trunk@84183 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- dandyid-services.php
+++ dandyid-services.php
@@ -4,7 +4,7 @@
 Plugin Name: DandyID Services
 Plugin URI: http://solidcode.com/
 Description: Retrieves your <a href="http://dandyid.org">DandyID</a> online identities and displays them as clickable links in your sidebar. After activating this Plugin: (1) Go to <a href="options-general.php?page=dandyid-services/dandyid-services.php">Settings -&gt; DandyID Services</a> to configure the required settings, and (2) Go to <a href="widgets.php">Design -&gt; Widgets</a> to add the DandyID Services sidebar widget to your sidebar.
-Version: 1.0.7
+Version: 1.0.8
 Author: Neil Simon
 Author URI: http://solidcode.com/
 */
@@ -45,13 +45,25 @@ define ('DANDYID_CACHE_DATE_OPTION', 'dandyID_cacheDateOption');
 define ('DANDYID_API_URL',           'http://www.dandyid.org/api/');
 
 
+// Global data -- used exclusively by dandyIDServices_xml*(), and dandyIDServices_refreshCache().
+// These globals are required, due to the way xml_*() callbacks are implemented by PHP.
+$gInsideSERVICE = FALSE;
+$gTag           = '';
+$gUrl           = '';
+$gSvcName       = '';
+$gSvcFavicon    = '';
+$gIndex         = 0;
+$gCacheOptions  = array (array ('url' => '', 'svcName' => '', 'svcFavicon' => ''));
+$gDandyID       = 0;
+
+
 function dandyIDServices_getTable ()
     {
     // If the cacheDate doesn't match the current date...
     if (get_option (DANDYID_CACHE_DATE_OPTION) != date ('Y-m-d'))
         {
         // Overwrite the existing cache, reset the cacheDate to current date)
-        dandyIDServices_refreshCacheFile ();
+        dandyIDServices_refreshCache ();
         }
 
     // Load existing settings options from wp database
@@ -148,80 +160,76 @@ function dandyIDServices_buildTable ()
     }
 
 
-function dandyIDServices_refreshCacheFile ()
+function dandyIDServices_refreshCache ()
     {
+    global $gInsideSERVICE, $gIndex, $gDandyID;
+
+    // Initialize XML-handler globals
+    $gIndex         = 0;
+    $gInsideSERVICE = FALSE;
+
     // Instantiate the dandyid class
-    $cDandyID = new dandyid ();
+    $gDandyID = new dandyid ();
 
     // Load existing options from wp database
     $dandyID_settingsOptions = get_option (DANDYID_SETTINGS_OPTIONS);
 
     // Set class API fields
-    $cDandyID->setAPIFields (DANDYID_API_KEY,
+    $gDandyID->setAPIFields (DANDYID_API_KEY,
                              DANDYID_API_TOKEN,
                              DANDYID_API_URL);
 
     // Set class user fields
-    $cDandyID->setUserFields ($dandyID_settingsOptions ['email_address'],
+    $gDandyID->setUserFields ($dandyID_settingsOptions ['email_address'],
                               $dandyID_settingsOptions ['email_address'],
                               $dandyID_settingsOptions ['password']);
 
     // Sync_user to freshen their data into the API repository.
-    $cDandyID->sync_user ();
+    $gDandyID->sync_user ();
 
     // Get the dandy services for the user -- returned as XML
-    if (($return_services_response = $cDandyID->return_services ()) == FALSE)
+    if (($return_services_response = $gDandyID->return_services ()) == FALSE)
         {
         // This can happen when the site is found, but the API is down
-        // Leave the existing cacheFile intact -- just exit
+        // Leave the existing cache intact -- just exit
         }
 
     elseif (strpos ($return_services_response, "Not Found"))
         {
         // This can happen when the site is NOT found
-        // Leave the existing cacheFile intact -- just exit
+        // Leave the existing cache intact -- just exit
         }
 
     else
         {
-        // Prepare to parse the XML
-        $return_services_xml = new SimpleXMLElement ($return_services_response);
+        global $gCacheOptions;
 
-        // Initialize row index for loading cache
-        $i = 0;
+        // Initialize global cache options array to null
+        $gCacheOptions  = array (array ('url' => '', 'svcName' => '', 'svcFavicon' => ''));
 
-        // Initialize cache options array to null
-        $cacheOptions = array (array ('url' => '', 'svcName' => '', 'svcFavicon' => ''));
+        // Prepare to parse
+        $xmlParser = xml_parser_create ();
 
-        // Parse values -- service by service
-        foreach ($return_services_xml->service as $service)
-            {
-            // Get the dandy service detail for the service
-            $service_details_response = $cDandyID->service_details ($service->svcId);
+        // Define XML callback -- called via xml_parse()
+        xml_set_element_handler ($xmlParser,
+                                 'dandyIDServices_xmlStartElement',
+                                 'dandyIDServices_xmlEndElement');
 
-            // Prepare XML return_services_response for parsing
-            $service_details_xml = new SimpleXMLElement ($service_details_response);
 
-            // Load next cache row (requires string cast to override storing a SimpleXMLElement object)
-            $cacheOptions [$i] ['url']        = (string) $service->url;
-            $cacheOptions [$i] ['svcName']    = (string) $service->svcName;
-            $cacheOptions [$i] ['svcFavicon'] = (string) $service_details_xml->service->svcFavicon;
+        // Define XML callback -- called via xml_parse()
+        xml_set_character_data_handler ($xmlParser, 'dandyIDServices_xmlData');
 
-            // Increment cache row index for next load
-            $i++;
-            }
+        // Parse the XML -- all data will be stored into gCacheOptions
+        xml_parse ($xmlParser, $return_services_response, TRUE);
 
-        // If at least one row was loaded...
-        if ($i > 0)
-            {
-            // Add a terminating row of null values
-            $cacheOptions [$i] ['url']        = '';
-            $cacheOptions [$i] ['svcName']    = '';
-            $cacheOptions [$i] ['svcFavicon'] = '';
-            }
+        // Set last item url in gCacheOptions to null
+        $gCacheOptions [$gIndex] ['url'] = '';
+
+        // Release parser resources
+        xml_parser_free ($xmlParser);
 
         // Store the cache options array to wp database
-        update_option (DANDYID_CACHE_OPTIONS, $cacheOptions);
+        update_option (DANDYID_CACHE_OPTIONS, $gCacheOptions);
 
         // Reset the cache date to the current date, store to wp database
         update_option (DANDYID_CACHE_DATE_OPTION, date ('Y-m-d'));
@@ -278,8 +286,8 @@ function dandyIDServices_updateSettingsOptionsPage ()
         // Store changed options back to wp database
         update_option (DANDYID_SETTINGS_OPTIONS, $dandyID_settingsOptions);
 
-        // Force the cache file to be created (or overwritten)
-        dandyIDServices_refreshCacheFile ();
+        // Force the cache to be created (or overwritten)
+        dandyIDServices_refreshCache ();
 
         // Display update message to user
         echo '<div id="message" class="updated fade"><p>' . "DandyID Service options saved successfully." . '</p></div>';
@@ -402,6 +410,80 @@ function dandyIDServices_addSubmenu ()
                       10,                                            // Access level all
                       __FILE__,                                      // This file displays the options page
                       'dandyIDServices_updateSettingsOptionsPage');  // Function that displays options page
+    }
+
+
+function dandyIDServices_xmlStartElement ($parser, $name, $attrs)
+    {
+    global $gInsideSERVICE, $gTag;
+
+    // This function gets called each time <xxxx> (any start element tag) encountered
+
+    // If inside a subtag of <service>...
+    if ($gInsideSERVICE)
+        {
+        // Possible values for gTag can be: SVCID, SVCNAME, USERSVCID, or SVCFAVICON
+        // NOTE: PHP converts the tags to uppercase
+        $gTag = $name;
+        }
+
+    // If not already within the <service> tag, check to see if this is a <service> tag
+    elseif ($name == 'SERVICE')
+        {
+        // Let dandyIDServices_xmlData() know that we're inside the SERVICE tag
+        $gInsideSERVICE = TRUE;
+        }
+    }
+
+
+function dandyIDServices_xmlEndElement ($parser, $name)
+    {
+    global $gInsideSERVICE, $gUrl, $gSvcName, $gSvcFavicon, $gCacheOptions, $gIndex;
+
+    // This function gets called each time ANY </xxxx> is encountered
+
+    // When xml_parser() detects </service>...
+    if ($name == 'SERVICE')
+        {
+        // Update global cache vars from global data fields
+        $gCacheOptions [$gIndex] ['url']        = $gUrl;
+        $gCacheOptions [$gIndex] ['svcName']    = $gSvcName;
+        $gCacheOptions [$gIndex] ['svcFavicon'] = $gSvcFavicon;
+
+        // Clear global data fields
+        $gUrl           = '';
+        $gSvcName       = '';
+        $gSvcFavicon    = '';
+        $gInsideSERVICE = FALSE;
+        $gIndex++;
+        }
+    }
+
+
+function dandyIDServices_xmlData ($parser, $data)
+    {
+    global $gInsideSERVICE, $gTag, $gUrl, $gSvcName, $gSvcFavicon;
+
+    // This function gets called iteratively, one time per each subtag with <service>
+
+    // Process the subtag -- save the set of values to globals
+    if ($gInsideSERVICE)
+        {
+        switch ($gTag)
+            {
+            case 'URL':
+                $gUrl .= $data;
+                break;
+
+            case 'SVCNAME':
+                $gSvcName .= $data;
+                break;
+
+            case 'SVCFAVICON':
+                $gSvcFavicon .= $data;
+                break;
+            }
+        }
     }
 
 
