c450ee4bedbdccbba132e02df5ca17e314a2cacd
nsimon <nsimon@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-01-18 16:23:04 +0000

    Added ability to View Change Log from settings page.
    git-svn-id: https://plugins.svn.wordpress.org/dandyid-services/trunk@87518 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- dandyid-services.php
+++ dandyid-services.php
@@ -4,7 +4,7 @@
 Plugin Name: DandyID Services
 Plugin URI: http://dandyid.org/
 Description: Retrieves your <a href="http://dandyid.org">DandyID</a> online identities and displays them as clickable links in your sidebar. After activating this Plugin: (1) Go to Settings -&gt; DandyID Services to configure the required settings, and (2) Go to Design -&gt; Widgets to add the DandyID Services sidebar widget to your sidebar.
-Version: 1.1.7
+Version: 1.1.8
 Author: Neil Simon, Sara Czyzewicz, Arron Kallenberg, Dan Perron, Anthony Dimitre
 Author URI: http://dandyid.org/
 */
@@ -413,6 +413,8 @@ function dandyIDServices_updateSettingsOptionsPage ()
       <p>&nbsp;&nbsp;<input type="submit" value="Save" /></p>
 
       </form>
+
+      <h5>&nbsp; &nbsp; ( <a href="http://wordpress.org/extend/plugins/dandyid-services/other_notes/">View Change Log</a> of past updates to the DandyID Services Plugin. )</h5>
 
       </div>';
     }
